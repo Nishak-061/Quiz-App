fe28a878b627c655c74392435761cc6ba62af50b
Nisha Kumari <nk.nishakumari71@gmail.com>
2025-06-17 18:35:53 +0530

    updates

--- frontend/quiz-app/vite.config.js
+++ frontend/quiz-app/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { resolve } from "path"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'public/_redirects',
-        dest: '.'
-      }
-    ]
-  })],
+  plugins: [react()],
 })
